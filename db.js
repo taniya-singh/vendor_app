8d72fca42e5639bbaee5087fcb615aab0b62691c
Taniya Singh <taniya.sk92@gmail.com>
2017-06-16 19:00:06 +0530

    mongodb path changed

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
-/*
- * The file will take care of the database connectivity
- */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/Vendor');
 
-//check if we are connected successfully or not
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-
-
+// mongoose.connect('mongodb://localhost/barberdo');
+mongoose.connect('mongodb://munchapp:munchapp2780@52.39.212.226:27017/munchapp',{auth:{authdb:"munchapp"}});
+mongoose.connection.on('error', function() {
+  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+  process.exit(1);
+});
 
 
 
