85867ad82e6ae1e0c39e35ce1c3543241d11db7b
Taniya Singh <taniya.sk92@gmail.com>
2017-07-17 13:07:13 +0530

    changes

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,6 @@ var userObj = require('./../app/controllers/users/users.js');
 	 router.get('/list',passport.authenticate('bearer', {session:true}), userObj.list);
 	 router.post('/add', userObj.add);
 	 router.post('/bulkUpdate', userObj.bulkUpdate);
-<<<<<<< HEAD
 	 router.post('/userlogin', passport.authenticate('userLogin'),userObj.userlogin);
 	 router.post('/update_vendor_info',userObj.update_vendor_info);
 	 router.post('/deleteUser', userObj.deleteUser);
@@ -17,22 +16,10 @@ var userObj = require('./../app/controllers/users/users.js');
      router.post('/faceBookLogin',userObj.faceBookLogin);
      router.post('/forgetpassword',userObj.forgetpassword);
      router.post('/reset_password',userObj.reset_password);
-=======
-	  router.post('/userlogin', passport.authenticate('userLogin'),userObj.userlogin);
-	  router.post('/update_vendor_info',userObj.update_vendor_info);
-	  router.post('/register', userObj.register);
-	  router.post('/deleteUser', userObj.deleteUser);
-	  router.get('/totalUser', userObj.totalUser);
-	  
-
-      router.post('/faceBookLogin',userObj.faceBookLogin);
-      router.post('/forgetpassword',userObj.forgetpassword);
-      router.post('/reset_password',userObj.reset_password);
-      router.post('/place_order',order.place_order);
+     //router.post('/place_order',order.place_order);
 
       //router.post('/userList',userObj.userList);
 
->>>>>>> fc3b5cafbcfe44e9c5a643e83bd70b65096e689d
 
 
 	 app.use('/users', router);
