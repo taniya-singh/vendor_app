2ce1be8ad5939927d5fe66522cdadbb404af0c40
Taniya <taniya.sk92@gmail.com>
2017-07-20 04:38:47 +0000

    changes of poonam in server

--- app/models/admin/signup_vendor.js
+++ app/models/admin/signup_vendor.js
@@ -13,7 +13,7 @@ var mySchema = new Schema({
   vendor_address:{type:String},
   city:{type:String},
   country:{type:String},
-  zipcode:{type:String},
+  zipCode:{type:String},
   latitude:{type:Number},
   longitude:{type:Number},
   geo:{type:[Number],index:'2dsphere'},
